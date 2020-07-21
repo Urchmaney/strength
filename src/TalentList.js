253484c59cdc8e48ebac76528d66d682a6aaa41c
Urchmaney <kingsobino@gmail.com>
2020-07-21 21:41:21 +0000

    Add talent list and talent component.

---
+++ src/TalentList.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+const TalentList = () => {
+  useEffect(() => {
+
+  });
+  return (
+    <div />
+  );
+};
---
+++ src/presentational/Talent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Talent = (props) => {
+  const {
+    
+  }
+}
