04676039cb100df874ed51210c86c9a4c5695eda
Urchmaney <kingsobino@gmail.com>
2020-07-22 15:28:17 +0000

    Updates

---
+++ precache-manifest.fef1fe3f9fdc6b7146c913500a9903b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d467ca1333c11848a71852ff9c188f",
+    "url": "/strength%20/index.html"
+  },
+  {
+    "revision": "5fa42ff6d4394902ebc8",
+    "url": "/strength%20/static/css/main.c08dfcf5.chunk.css"
+  },
+  {
+    "revision": "0df70ee59f1528e83943",
+    "url": "/strength%20/static/js/2.8cbdafbe.chunk.js"
+  },
+  {
+    "revision": "2e4f31804d72137a9d9b13db1a0e4557",
+    "url": "/strength%20/static/js/2.8cbdafbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa42ff6d4394902ebc8",
+    "url": "/strength%20/static/js/main.325faa03.chunk.js"
+  },
+  {
+    "revision": "1f1c96f44aa0583f3e14",
+    "url": "/strength%20/static/js/runtime-main.7eed98a2.js"
+  }
+]);
